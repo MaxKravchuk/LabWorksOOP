24907f52e10b8b868068c03a7967030909b589d7
Максим <Flinki2002@gmail.com>
2020-02-13 11:39:29 +0200

    first labWork

---
+++ BityOper.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int Decrease(int);
+void HiB(int, int);
+
+int main()
+{
+    cout << "\tKravchuk Maxim\n\tIS-92\n";
+    int a;
+    int b;
+    cout<<"Enter First and Second digits\n";
+    cin >> a;
+    cin >> b;
+    HiB(a, b);
+    cout<<a<<" - 1 = "<< Decrease(a)<<endl<<b<<" - 1 = "<<Decrease(b);
+}
+int Decrease(int a)
+{
+    int b = ~- a;
+    return b;
+}
+void HiB(int a, int b)
+{
+    int ans = b & ((a - b) >> 31) | a & (~(a - b) >> 31);
+    cout<<ans<<" is bigger\n";
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BityOpCS
+{
+    class Program
+    {
+        static int Decrease(int a)
+        {
+            int b = ~-a;
+            return b;
+        }
+        static void HiB(int a, int b)
+        {
+            int ans = b & ((a - b) >> 31) | a & (~(a - b) >> 31);
+            Console.WriteLine("{0} is bigger",ans);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tKravchuk Maxim\n\tIS-92");
+            Console.WriteLine("Enter First and Second digits");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            HiB(a, b);
+            Console.WriteLine("{0} - 1 = {1}\n{2} - 1 = {3}", a, Decrease(a), b, Decrease(b));
+        }
+    }
+}
