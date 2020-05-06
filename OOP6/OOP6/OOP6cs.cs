b8fbe84b237d2f3b7b819371c19b42334e8087bd
MaxKravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2020-05-06 09:24:07 +0300

    LabWork 6
    LabWork 6

---
+++ OOP6/OOP6/OOP6cs.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace OOP6
+{
+    class Exr
+    {
+        private double _a, _b, _c, _d;//змінні
+
+        public Exr(double a, double b, double c, double d)//конструктор
+        {
+            _a = a; _b = b; _c = c; _d = d;
+        }
+
+        private double SolveSqrt()//метод визначення кореня
+        {
+            if ((24 + _d - _c) < 0)
+            {
+                throw new ArithmeticException();
+            }
+            return Math.Sqrt(24 + _d - _c);
+        }
+
+        private double SolveDen()//метод визначення знаменника рівняння
+        {
+            double a = SolveSqrt() + (_a / _b);
+            if (_b == 0 || a==0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+                return SolveSqrt() + (_a / _b);
+        }
+
+        public double Answer()//метод підрахунку відповіді
+        {
+            return ((1 + _a - _b / 2) / SolveDen());
+        }
+        public void Checker()//метод перевірки виключень
+        {
+            try
+            {
+                SolveSqrt();
+                try
+                {
+                    SolveDen();
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("\n\nWARNING!!!____Error! You cannot divide by 0____WARNING!!!\n\n");
+                }
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("\n\nWARNING!!!____Error! Negative number under the sqrt____WARNING!!!\n\n");
+            }
+        }
+        class OOP6cs
+        {
+            static void Main(string[] args)
+            {
+                double a, b, c, d;
+                Console.WriteLine("\tMaxim Kravchuk\n\tIS-92");
+                Console.Write("Enter an amount of exercises -> ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Exr[] exercise = new Exr[n];//створення масиву об'єктів
+                for(int i = 0; i<n;i++)//заповнення об'єктів
+                {
+                    Console.WriteLine("{0} try", (i+1));
+                    Console.Write("Enter A -> ");
+                    a = Convert.ToInt32(Console.ReadLine());//ініціалізація змінних
+                    Console.Write("Enter B (B!=0) -> ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter C -> ");
+                    c = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter D -> ");
+                    d = Convert.ToInt32(Console.ReadLine());
+                    exercise[i] = new Exr(a, b, c, d);//ініціалізація об'єкта
+                    exercise[i].Checker();
+                    Console.WriteLine("Resulr is -> " + Math.Round(exercise[i].Answer(),4));//вивід відповіді
+                }
+            }
+        }
+    }
+}
---
+++ OOP6/OOP6pp/OOP6pp.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include<cstdio>
+using namespace std;
+
+class Exr
+{
+private:
+    double _a, _b, _c, _d; //змінні
+    double SolveSqrt()//метод визначення кореня
+    {
+        try
+        {
+            if ((24 + _d - _c) < 0)
+                throw 1;
+            else
+            {
+                return sqrt(24 + _d - _c);
+            }
+        }
+        catch (int i)
+        {
+            cout << "\n\nWARNING!!!____Error! Negative number under the sqrt____WARNING!!!\n\n";
+            exit(0);
+        }
+    }
+    double SolveDen()//метод визначення знаменника рівняння
+    {
+        int a = SolveSqrt() + (_a / _b);
+        try
+        {
+            if (_b == 0)
+                throw 1;
+            if (a == 0)
+                throw 1.0;
+            else
+            {
+                return SolveSqrt() + (_a / _b);
+            }
+        }
+        catch (int i)
+        {
+            cout << "\n\nWARNING!!!____Error! B cannot be 0____WARNING!!!\n\n";
+            exit(0);
+        }
+        catch (double i)
+        {
+            cout << "\n\nWARNING!!!____You cannot devide by 0____WARNING!!!\n\n";
+            exit(0);
+        }
+    }
+public:
+    Exr(){};//конструктор
+    Exr(double a, double b, double c, double d)//конструктор
+    {
+        _a = a; _b = b; _c = c; _d = d;
+    }
+    double Answer()//метод підрахунку відповіді
+    {
+        return ((1 + _a - _b / 2) / SolveDen());
+    }
+};
+
+int main()
+{
+    cout << "\tKravchuk Maxim\n\tIS-92\n";
+    double a, b, c, d;
+    int size;
+    cout << "Enter an amount of exercises -> ";
+    cin >> size;
+    Exr* exercise = new Exr[size];//створення масиву об'єктів
+    for (int i = 0; i < size; i++)//заповнення об'єктів
+    {
+        cout << i << "----------\n";
+        cout << "Enter A -> ";
+        cin >> a;//ініціалізація змінних
+        cout << "Enter B (B!=0) -> ";
+        cin >> b;
+        cout << "Enter C -> ";
+        cin >> c;
+        cout << "Enter D -> ";
+        cin >> d;
+        exercise[i] = Exr(a, b, c, d);//ініціалізація об'єкта
+        cout << "Result is -> " << setw(5) << setprecision(2) << exercise[i].Answer()<<endl;//вивід відповіді
+    }
+}
