cfb4202398d9719b21e26fc93f35028aa2488341
MaxKravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2020-06-03 10:32:29 +0300

    Lab work 7

---
+++ OOP7/OOP7/OOP7.cpp
@@ -0,0 +1,238 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Elem//Структура комірки
+{
+	long data;//поле-значення
+	Elem* next,//показчик на наступний ел
+		* prev;//показчик на попередній ел
+};
+
+class List//клас список
+{
+	Elem* Head,//поле-голова
+		* Tail;//поле-хвіст
+	int ElementsCounter;//поле-розмір списку
+
+public:
+
+	//конструктор
+	List()
+	{
+		Head = Tail = NULL;
+		ElementsCounter = 0;
+	}
+
+	//ф-я додавання в кінець
+	void AddToTail(long Item) 
+	{
+		Elem* temp = new Elem;
+		temp->next = 0;
+		temp->data = Item;
+		temp->prev = Tail;
+
+		if (Tail != NULL) Tail->next = temp;
+		if (ElementsCounter == NULL) Head = Tail = temp;
+		else Tail = temp;
+
+		ElementsCounter++;
+	} 
+
+	//ф-я виводу списку
+	void ShowList() 
+	{
+		if (ElementsCounter != 0)
+		{
+			Elem* temp = Head;
+			cout << "| ";
+			while (temp->next != NULL)
+			{
+				cout << temp->data << " | ";
+				temp = temp->next;
+			}
+
+			cout << temp->data << " |\n";
+		}
+		else
+		{
+			cout << "No items in list\n";
+		}
+	}
+
+	//ф-я підрахунку к-сті елементів, що % на 5 і стоять на парних поз
+	int GetElemWhithMulOf5()
+	{
+		int cnt = 0;
+		for (int i = 1; i < ElementsCounter; i = i + 1)
+		{
+			long _tempdata = GetItem(i);
+			if (_tempdata % 5 == 0)
+			{
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	//ф-я видалення ел, що більші за середнє значення
+	void DelItemsWBiggerTAvg()
+	{
+		int avg = GetAnAvgOfAllItems();
+		Elem* temp = Head;
+		int _tempSize = ElementsCounter;
+		int i = 1;
+		while (i <= _tempSize)
+		{
+			if (temp->data > avg)
+			{
+				temp = temp->next;
+				DelFunc(i);
+				_tempSize--;
+			}
+			else
+			{
+				i++;
+				if (temp->next != NULL)
+					temp = temp->next;
+			}
+		}
+	}
+
+private://допоміжні ф-ї
+
+	//ф-я видалення за індексом
+	void DelFunc(int k)
+	{
+
+		if (k == 1)
+		{
+			//Elem* temp = Head;
+			Head = Head->next;
+			delete(Head->prev);
+			//ElementsCounter--;
+		}
+
+		if (k == ElementsCounter)
+		{
+			//Elem* temp = Tail;
+			Tail = Tail->prev;
+			Tail->next = NULL;
+			delete(Tail->next);
+			//ElementsCounter--;
+		}
+
+		if (k > 1 && k < ElementsCounter)
+		{
+			int i = 1;
+
+			Elem* Del = Head;
+
+			while (i < k)
+			{
+				// Доходим до элемента, 
+				// который удаляется
+				Del = Del->next;
+				i++;
+			}
+
+			// Доходим до элемента, 
+			// который предшествует удаляемому
+			Elem* PrevDel = Del->prev;
+			// Доходим до элемента, который следует за удаляемым
+			Elem* AfterDel = Del->next;
+
+			// Если удаляем не голову
+			if (PrevDel != 0 && ElementsCounter != 1)
+				PrevDel->next = AfterDel;
+			// Если удаляем не хвост
+			if (AfterDel != 0 && ElementsCounter != 1)
+				AfterDel->prev = PrevDel;
+			delete Del;
+			//ElementsCounter--;
+		}
+	}
+
+	//ф-я пошуку елементу за індексом
+	long GetItem(int pos)
+	{
+		// Позиция от 1 до Count?
+		if (pos < 1 || pos > ElementsCounter)
+		{
+			// Неверная позиция
+			cout << "Incorrect position !!!\n";
+			return 0;
+		}
+
+		Elem* temp;
+
+		// Определяем с какой стороны 
+		// быстрее двигаться
+		if (pos <= ElementsCounter / 2)
+		{
+			// Отсчет с головы
+			temp = Head;
+			int i = 1;
+
+			while (i < pos)
+			{
+				// Двигаемся до нужного элемента
+				temp = temp->next;
+				i++;
+			}
+		}
+		else
+		{
+			// Отсчет с хвоста
+			temp = Tail;
+			int i = 1;
+
+			while (i <= ElementsCounter - pos)
+			{
+				// Двигаемся до нужного элемента
+				temp = temp->prev;
+				i++;
+			}
+		}
+		return temp->data;
+	}
+
+	//ф-я похуку середнього значення
+	int GetAnAvgOfAllItems()
+	{
+		int Sum = 0;
+		int AvgSum = 0;
+		Elem* temp = Head;
+		while (temp != 0)
+		{
+			Sum += temp->data;
+			temp = temp->next;
+		}
+		AvgSum = Sum / ElementsCounter;
+		return AvgSum;
+	}
+
+};
+
+int main()
+{
+	cout << "Maxim Kravchuk\nIS-92\nC++\n\n";
+
+	List l;//ініціалізую список
+
+	long a[5] = { 88,95,93,55,64 };
+
+	for (int i = 0; i < 5; i++)//заповнюю список
+	{
+		l.AddToTail(a[i]);
+	}
+
+	cout << "List l:\n" << endl;
+	l.ShowList();
+	cout << endl;
+	cout << "The number of elements multiples of 5 that located in pairs -> " << l.GetElemWhithMulOf5() << endl << endl;
+
+	l.DelItemsWBiggerTAvg();
+	cout << "List l after deleting items that are larger than average\n\n";
+	l.ShowList();
+}
---
+++ OOP7/OOP7cs/OOP7cs.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.VisualBasic.FileIO;
+
+namespace OOP7cs
+{
+    public class Elem//клас комірка
+    {
+        private long _Data;//поле-значення
+        private Elem _Next;//показчик на наступний ел
+        private Elem _Prev;//показчик на попередній ел
+
+        //всластивосі звернення до приватних полей
+        public long Value
+        {
+            get { return _Data; }
+            set { _Data = value; }
+        }
+        public Elem(long Data)
+        {
+            this._Data = Data;
+        }
+        public Elem Next
+        {
+            get { return this._Next; }
+            set { this._Next = value; }
+        }
+        public Elem Prev
+        {
+            get { return this._Prev; }
+            set { this._Prev = value; }
+        }
+
+    }
+
+    class List//клас список
+    {
+
+        private Elem Head;//поле-голова
+        private Elem Current;//поле-поточний ел
+        private Elem Tail;//поле-хвіст
+        private int size;//поле-розмір списку
+
+        public int Count
+        {
+            get { return size; }
+            set { size = value; }
+        }//властивість для розміру
+
+        public List()
+        {
+            size = 0;
+            Head = Current = Tail = null;
+        }//конструктор
+
+        public void AddToTail(long newElement)
+        {
+            Elem newElem = new Elem(newElement);
+
+            if (Head == null)
+            {
+                Head = Tail = newElem;
+            }
+            else
+            {
+                Tail.Next = newElem;
+                newElem.Prev = Tail;
+                Tail = newElem;
+            }
+            Count++;
+        }//ф-я додавання в кінець
+
+        public void ShowList()
+        {
+            if (Count != 0)
+            {
+                Current = Head;
+                Console.Write("| ");
+                while (Current != null)
+                {
+                    Console.Write(Current.Value + " | ");
+                    Current = Current.Next;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No items in list");
+            }
+        }//ф-я виводу списку
+
+        public int GetElemWhithMulOf5()
+        {
+            int cnt = 0;
+            for(int i = 1; i<=Count;i=i+2)
+            {
+                long _tempdata = GetItem(i);
+                if (_tempdata % 5 == 0)
+                {
+                    cnt++; 
+                }
+            }
+            return cnt;
+        }//ф-я підрахунку к-сті елементів, що % на 5 і стоять на парних поз
+
+        public void DelItemsWBiggerTAvg()
+        {
+            int avg = GetAnAvgOfAllItems();
+            Current = Head;
+            int _tempsize = Count;
+            int i = 1;
+            while(i<=_tempsize)
+            {
+                if(Current.Value > avg)
+                {
+                    Current = Current.Next;
+                    DelFunc(i);
+                    _tempsize--;
+                }
+                else
+                {
+                    i++;
+                    if (Current.Next != null) Current = Current.Next;
+                }
+            }
+        }//ф-я видалення ел, що більші за середнє значення
+
+        //допоміжні ф-ї
+
+        private long GetItem(int pos)
+        {
+            if (pos < 1 || pos > Count)
+                return 0;
+            if (pos <= Count / 2)
+            {
+                Current = Head;
+                int i = 1;
+                while (i < pos)
+                {
+                    Current = Current.Next;
+                    i++;
+                }
+            }
+            else
+            {
+                Current = Tail;
+                int i = 0;
+                while (i <= Count - pos)
+                {
+                    Current = Current.Prev;
+                    i++;
+                }
+            }
+            return Current.Value;
+        }//ф-я пошуку елементу за індексом
+
+        private void DelFunc(int index)
+        { //удалить элемент по индексу
+            if (index < 1 || index > size)
+            {
+                throw new InvalidOperationException();
+            }
+            else if (index == 1)
+            {
+                Head = Head.Next;
+                Head.Prev = null;
+
+            }
+            else if (index == size)
+            {
+                Tail = Tail.Prev;
+                Tail.Next = null;
+            }
+            else if (index > 1 && index < Count)
+            {
+                uint i = 1;
+                Current = Head;
+                Elem Del = Head;
+
+                while (i < index)
+                {
+                    Del = Del.Next;
+                    i++;
+                }
+                Elem PrevDel = Del.Prev;
+                Elem AfterDel = Del.Next;
+                if (PrevDel != null && Count != 1)
+                    PrevDel.Next = AfterDel;
+                if (AfterDel != null && Count != 1)
+                    AfterDel.Prev = PrevDel;
+                Del = null;
+            }
+        }//ф-я видалення за індексом
+
+        private int GetAnAvgOfAllItems()
+        {
+            int Sum = 0, AvgSum=0;
+            Current = Head;
+            while(Current!=null)
+            {
+                Sum += (int)Current.Value;
+                Current = Current.Next;
+            }
+            AvgSum = Sum / Count;
+            return AvgSum;
+        }//ф-я похуку середнього значення
+
+    }
+
+    class OOP7cs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maxim Kravchuk\nIS-92\nC#\n\n");
+
+            List L = new List();//ініціалізую список
+
+            long[] a = new long[5] { 88, 95, 93, 55, 64 };
+
+            for(int i = 0; i<5; i++)//заповнюю список
+            {
+                L.AddToTail(a[i]);
+            }
+
+            Console.WriteLine("List L:\n");
+            L.ShowList();
+            Console.WriteLine();
+
+            Console.WriteLine($"The number of elements multiples of 5 that located in pairs -> {L.GetElemWhithMulOf5()}\n\n");
+
+            L.DelItemsWBiggerTAvg();
+            Console.WriteLine("List l after deleting items that are larger than average\n\n");
+            L.ShowList();
+        }
+    }
+}
